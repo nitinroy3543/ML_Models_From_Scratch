6c52e2b5861769e55e870becd4c0452ec3a9c33d
nitinroy3543 <36908483+nitinroy3543@users.noreply.github.com>
2022-10-12 20:36:16 -0700

    Added linear regression using gradient descent algorithm

---
+++ linear_regression_test.py
@@ -0,0 +1,31 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+X, y = datasets.make_regression(n_samples=100, n_features=1,
+                                noise = 20, random_state=1234)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+# fig = plt.figure(figsize=(8,6))
+# plt.scatter(X[:,0], y, color = "b", marker = "o", s=30)
+# #plt.show()
+
+# print(X_train.shape)
+# print(y_train.shape)
+
+from linear_regression import LinearRegression
+
+regressor = LinearRegression(lr=0.01)
+regressor.fit(X_train, y_train)
+predicted = regressor.predict(X_test)
+
+def mse(y_true, y_predicted):
+    return np.mean((y_true-y_predicted)**2)
+
+mse_value = mse(y_test, predicted)
+plt.scatter(y_test,predicted)
+plt.show()
+print(mse_value)
+
+
